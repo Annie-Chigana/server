344371ca36b42757b9f853a50a8ccbbaa0092c57
Anni Chiganangana <48432614+Annie-Chigana@users.noreply.github.com>
2026-02-02 10:05:30 +0200

    update

--- server-app/src/pages/Hello.js
+++ server-app/src/pages/Hello.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function Hello() {
+
+    useEffect(() => {
+        fetch("http://localhost:3001")
+        .then(res => res.text())
+        .then(data => console.log(data));
+    }, []);
+
     return (
         <div>
             <h1>Hello World</h1>
