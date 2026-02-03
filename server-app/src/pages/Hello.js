c5597f15968f3b9fdbd922a87a829cd863da7e87
Anni Chiganangana <48432614+Annie-Chigana@users.noreply.github.com>
2026-02-03 12:30:22 +0200

    Add CORS middleware and update dependencies for improved API handling

--- server-app/src/pages/Hello.js
+++ server-app/src/pages/Hello.js
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Hello() {
+
+    const [message, setMessage] = useState("");
+
 
     useEffect(() => {
         fetch("http://localhost:3001")
         .then(res => res.text())
-        .then(data => console.log(data));
+        .then(data => setMessage(data))
+        .catch(err => console.error(err));
     }, []);
 
     return (
         <div>
             <h1>Hello World</h1>
+            <p>{message}</p>
         </div>
     )
 }
